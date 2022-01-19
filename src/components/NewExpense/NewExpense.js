b8ad4243737611b4a22b1f280a688bc0d39ee612
Frank Bishop <ffbishop3@gmail.com>
2022-01-18 20:31:14 -0600

    assignment-4 finished

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,34 @@
 import "./NewExpense.css";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [formOpen, setFormStatus] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setFormStatus(false);
+  };
+
+  const openForm = () => {
+    setFormStatus(true);
+  };
+
+  const closeForm = () => {
+    setFormStatus(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!formOpen && <button onClick={openForm}>
+        Add New Expense
+      </button>}
+      {formOpen && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} closeForm={closeForm} />}
     </div>
   );
 };
